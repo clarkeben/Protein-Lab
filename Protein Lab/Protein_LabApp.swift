7f216ab9142501454da6ef4bd79865350faaa65a
Ben <benclarke023@gmail.com>
2024-01-19 19:40:02 +0000

    Initial Commit

---
+++ Protein Lab/Protein_LabApp.swift
@@ -0,0 +1,17 @@
+//
+//  Protein_LabApp.swift
+//  Protein Lab
+//
+//  Created by Ben Clarke on 19/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Protein_LabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
